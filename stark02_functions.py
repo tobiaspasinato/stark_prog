4bbf634f7c533e59f2352ff32c668cede1371ada
Tobías Pasinato <123979610+tobiaspasinato@users.noreply.github.com>
2023-05-01 23:08:31 -0300

    0.4

--- stark02_functions.py
+++ stark02_functions.py
@@ -88,14 +88,54 @@ def calcular_max_min_dato(heroes:list, calculo:str, key:str):
         calcular_min(heroes, key)
 
 def stark_calcular_imprimir_heroe(heroes:list, calculo:str, key:str):
-    lista_vacia = 0
+    valor_lista = 0
     if len(heroes) == 0:
-        lista_vacia == -1
-        return lista_vacia
+        valor_lista == -1
+        return valor_lista
     elif len(heroes) != 0:
-        lista_vacia == 1
+        valor_lista == 1
         imprimirRespuesta(calcular_max_min_dato(heroes, calculo, key))
 
+def sumar_dato_heroe(heroes:list, key:str):
+    valor_lista = 0
+    acumulador_heroes = 0
+    if len(heroes) == 0:
+        valor_lista = -1
+        return valor_lista
+    else:
+        for heroe in heroes:
+            acumulador_heroes = acumulador_heroes + heroe[key]
+        return acumulador_heroes
+    
+def dividir(dividendo:int, divisor:int):
+    if divisor == 0:
+        return 0
+    else:
+        resultado = dividendo / divisor
+        return resultado
 
+def calcular_promedio(heroes:list, key:str):
+    divisor = len(heroes)
+    resultado = dividir(sumar_dato_heroe(heroes, "altura"), divisor)
+    return resultado
 
+def stark_calcular_imprimir_promedio_altura(heores:list, key:str):
+    valor_lista = 0
 
+    if len(heores) == 0:
+        valor_lista = -1
+        return valor_lista
+    else:
+        imprimirRespuesta(calcular_promedio(heores, key))
+
+def imprimir_menu():
+    menu = """\nCual opción va a elegir?
+    \n1 - El nombre de todos los héroes
+    \n2 - Altura de todos los heroes
+    \n3 - Heroe mas alto
+    \n4 - Heroe mas chico
+    \n5 - Promedio Alturas
+    \n0 - Salir del Programa
+    """
+
+    imprimirRespuesta(menu)
--- stark02_menu.py
+++ stark02_menu.py
@@ -5,6 +5,9 @@ while True:
     print("""\nCual opción va a elegir?
     \n1 - El nombre de todos los héroes
     \n2 - Altura de todos los heroes
+    \n3 - Heroe mas alto
+    \n4 - Heroe mas chico
+    \n5 - Promedio Alturas
     \n0 - Salir del Programa
     """)
     
@@ -18,5 +21,14 @@ while True:
         stark_imprimir_nombres_alturas(lista_personajes)
     elif opcion == 3:
         stark_calcular_imprimir_heroe(lista_personajes, "maximo", "altura")
+    elif opcion == 4:
+        stark_calcular_imprimir_heroe(lista_personajes, "minimo", "altura")
+    elif opcion == 5:
+        stark_calcular_imprimir_promedio_altura(lista_personajes, "altura")
+    elif opcion == 6:
+        stark_calcular_imprimir_heroe(lista_personajes, "maximo", "peso")
+    elif opcion == 7:
+        stark_calcular_imprimir_heroe(lista_personajes, "minimo", "peso")
+
 system("cls")
 print("Fin del Programa")
